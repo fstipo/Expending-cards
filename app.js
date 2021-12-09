1f5240086ddfa20c7af41ad69630ce48ea2ef03e
fstipo <fstipanov@gmail.com>
2021-12-09 11:23:34 +0100

    DOM elements

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+const divContainer = document.createElement('div');
+divContainer.classList.add('container');
+document.body.appendChild(divContainer);
+
+for (let i = 0; i < 5; i++) {
+  const divPanelEl = document.createElement('div');
+  divPanelEl.classList.add('panel');
+  divPanelEl.setAttribute(
+    'style',
+    "background-image: url('https://picsum.photos/id/960/600')"
+  );
+  divContainer.appendChild(divPanelEl);
+  const h3 = document.createElement('h3');
+  h3.classList.add('panel-title');
+  h3.textContent = 'Explore the world';
+  divPanelEl.appendChild(h3);
+}
+
 const panels = document.querySelectorAll('.panel');
 
 const removeActiveClass = () => {

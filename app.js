4d46ff517d4f18da7090f8fed9d545f8990d2824
fstipo <fstipanov@gmail.com>
2021-12-17 09:19:56 +0100

    the end

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ const container = document.createElement('div');
 container.classList.add('container');
 document.body.appendChild(container);
 
+const imgID = [960, 666, 525, 901, 685];
+const imgTxt = [
+  'Explore the world',
+  'Wild Forest',
+  'Sahara sand',
+  'Winter',
+  'Mountains clouds',
+];
 for (let i = 0; i < 5; i++) {
+  const bgImg = `background-image: url('https://picsum.photos/id/${imgID[i]}/600')`;
   const panelEl = document.createElement('div');
   panelEl.classList.add('panel');
-  panelEl.setAttribute(
-    'style',
-    "background-image: url('https://picsum.photos/id/960/600')"
-  );
+  panelEl.setAttribute('style', bgImg);
+  console.log(bgImg);
   // panelEl[0].classList.add('active');
   container.appendChild(panelEl);
   const h3 = document.createElement('h3');
   h3.classList.add('panel-title');
-  h3.textContent = 'Explore the world';
+  h3.textContent = imgTxt[i];
   panelEl.appendChild(h3);
 }
 
